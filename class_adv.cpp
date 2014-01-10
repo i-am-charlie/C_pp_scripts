27051ab61737d12c8d86668da081d925233b3e8d
i.am.charlie <charlesv@rams.colostate.edu>
2014-01-10 12:39:29 -0700

    class w/ constructors as an adv data type

--- class_adv.cpp
+++ class_adv.cpp
@@ -1,5 +1,6 @@
 //Slightly more advanced demonstration of classes
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class BankAccount
@@ -25,69 +26,74 @@ public:
 	void output(ostream& outs);
 
 private:
-	double balance;
-	double interest_rate;
+	double dollars_part;
+	double cents_part;
+	double interest_rate; //expressed as a fraction, e.g. 0.057 for 5.7%
 
 	double fraction(double percent);
 	//simply converts a percent to a fraction  ex. fraction(50.3) = .503
+	
+	double percent(double fraction_value);
+	//converts a fraction to a percent.  ex. percent(0.503) returns 50.3%
 };
 
 int main()
 {
 	BankAccount account1(100, 2.3), account2;
-	cout << "Start of Test:\n";
 	
-	account2.set(123,99,3.0);
-	cout << "account2 initial statement:\n";
-	account2.output(cout);
-
 	cout << "account1 initialized as follows:\n";
 	account1.output(cout);
-
-	account1.update();
-	cout << "account1 after accumulating interest will be: \n";
-	account1.output(cout);
-
-	account2 = account1;
-	cout << "account2:\n";
+	cout << "account2 initialized as follows:\n";
 	account2.output(cout);
+
+	account1 = BankAccount(999, 99, 5.5);
+	cout << "account1 reset to the following: \n";
+	account1.output(cout);
 
 	return 0;
 }
 
 BankAccount::BankAccount()
 {
- 	balance = 0.00;
+	dollars_part = 0;
+	cents_part = 0;
 	interest_rate = 0.00;
 }
 
 BankAccount::BankAccount(int dollars, int cents, double rate)
 {
-	balance = dollars + 0.01*cents;
-	interest_rate = rate;
+	dollars_part = dollars;
+	cents_part = cents;
+	interest_rate = fraction(rate);
 }
 
 BankAccount::BankAccount(int dollars, double rate)
 {
-	balance = dollars + 0.00;
-	interest_rate = rate;
+	dollars_part = dollars;
+	cents_part = 0;
+	interest_rate = fraction(rate);
 }
 
 void BankAccount::set(int dollars, int cents, double rate)
 {
-	balance = dollars + 0.01*cents;
+	dollars_part = dollars;
+	cents_part = cents;
 	interest_rate = rate;
 }
 
 void BankAccount::set(int dollars, double rate)
 {
-	balance = dollars;
+	dollars_part = dollars;
+	cents_part = 0;
 	interest_rate = rate;
 }
 
 void BankAccount::update()
 {
-	balance = balance + fraction(interest_rate)*balance;
+	double balance = get_balance();
+	balance = balance + interest_rate*balance;
+	dollars_part = floor(balance);
+	cents_part = floor((balance-dollars_part)*100);
 }
 
 double BankAccount::fraction(double percent)
@@ -95,14 +101,18 @@ double BankAccount::fraction(double percent)
 	return (percent/100.0);
 }
 
+double BankAccount::percent(double fraction_value)
+{
+	return (fraction_value*100.0);
+}
 double BankAccount::get_balance()
 {
-	return balance;
+	return (dollars_part + 0.01*cents_part);
 }
 
 double BankAccount::get_rate()
 {
-	return interest_rate;
+	return percent(interest_rate);
 }
 
 void BankAccount::output(ostream& outs)
@@ -110,6 +120,6 @@ void BankAccount::output(ostream& outs)
 		outs.setf(ios::fixed);
 		outs.setf(ios::showpoint);
 		outs.precision(2);
-		outs << "Account balance $" << balance << endl;
-		outs << "Interest rate " << interest_rate << "%" << endl;
+		outs << "Account balance $" << get_balance() << endl;
+		outs << "Interest rate " << get_rate() << "%" << endl;
 }
