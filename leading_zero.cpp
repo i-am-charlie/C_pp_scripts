929958d2d490d364912a4802f45bf5c9dedbad42
i.am.charlie <charlesv@rams.colostate.edu>
2014-01-18 21:45:55 -0700

    small test program

---
+++ leading_zero.cpp
@@ -0,0 +1,14 @@
+//this program checks whether the compiler interprets input with a leading '0'
+//as a base-eight numeral... which mine didn't
+//i think this is some sort of a really old-school convention
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cin >> x;
+	cout << x << endl;
+	return 0;
+}
